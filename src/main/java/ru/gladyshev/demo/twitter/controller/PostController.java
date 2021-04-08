d52f46f6dcc9c038933968fbd17353097e25c715
Ivan Gladyshev <ivglad007@gmail.com>
2021-04-08 22:15:50 +0300

    Creating web part of application including controllers and view

--- src/main/java/ru/gladyshev/demo/twitter/controller/PostController.java
+++ src/main/java/ru/gladyshev/demo/twitter/controller/PostController.java
@@ -1,16 +1,25 @@
 package ru.gladyshev.demo.twitter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.nio.channels.FileChannel;
 
 @Controller
 public class PostController {
 
 
-    @GetMapping("/")
-    public String hello(){
+    @GetMapping("/hello")
+    public String hello(ModelMap model){
+        model.put("value", "HELLO!!");
         return "hello";
     }
 
+
+    @GetMapping("/")
+    public String index(ModelMap model){
+        return "index";
+    }
 
 }
